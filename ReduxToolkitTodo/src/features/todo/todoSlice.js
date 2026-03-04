975d3821b9b3fd6fb0c583f4920ebd9154125b78
Sai Shashwath AR <saishashwath2004@gmail.com>
2026-03-04 23:12:38 +0530

    redux toolkit update

--- ReduxToolkitTodo/src/features/todo/todoSlice.js
+++ ReduxToolkitTodo/src/features/todo/todoSlice.js
@@ -2,7 +2,7 @@ import {createSlice,nanoid} from '@reduxjs/toolkit';
 
 const initialState = {
     todos:[{id:1 ,text:"Hello world"}]
-}
+} //this tells redux , start the store with this data
 
 export const todoSlice=createSlice({
     name:"tod",
@@ -19,5 +19,17 @@ export const todoSlice=createSlice({
         removeTodo: (state,action)=>{
             state.todos=state.todos.filter((todo)=>todo.id!==action.payload)
         },
+        updateTodo:(state,action)=>{
+            const todo=state.todos.find((todo)=>todo.id===action.payload.id)
+            if(todo)
+            {
+                todo.text=action.payload.text
+            }
+
+        }
     }
 })
+
+export const {addTodo,removeTodo,updateTodo} = todoSlice.actions
+
+export const todoReducer= todoSlice.reducer
