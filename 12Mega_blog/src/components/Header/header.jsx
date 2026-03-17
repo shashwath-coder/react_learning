ddd2445f99553220cdf035e50f2c418c761809dc
Sai Shashwath AR <saishashwath2004@gmail.com>
2026-03-18 02:47:26 +0530

    updates on blog

--- 12Mega_blog/src/components/Header/header.jsx
+++ 12Mega_blog/src/components/Header/header.jsx
@@ -1,10 +1,76 @@
 import React from 'react'
+import { Container,Logo,Logout_btn } from '../index'
+import { useSelector } from 'react-redux'
+import {useNavigate} from "react-router-dom"
 
 function Header() {
+  const authStatus= useSelector((state)=>state.auth.status)
+
+  const navigate= useNavigate()
+
+  const nav_items=[
+    {
+      name:"Home",
+      url:'/',
+      active: true
+    },
+    {
+      name: "Login",
+      url: "/login",
+      active: !authStatus,
+  },
+  {
+      name: "Signup",
+      url: "/signup",
+      active: !authStatus,
+  },
+  {
+      name: "All Posts",
+      url: "/all-posts",
+      active: authStatus,
+  },
+  {
+      name: "Add Post",
+      url: "/add-post",
+      active: authStatus,
+  },
+  ]
   return (
-    <div>
-      <h1>header</h1>
-    </div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div>
+            <Link to='/'>
+          <Logo width='70px'   />
+          </Link>
+          </div>
+
+          <ul className='flex ml-auto'>
+            {nav_items.map((item)=>
+              item.active?
+              (
+                <li key='item.name'>
+                  <button onClick={()=>navigate(item.url)}
+                    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                    >
+                    {item.name}
+                  </button>
+                </li>
+              )
+              (
+                
+              ):null
+            )}
+            {authStatus &&(
+             <div>
+              <Logout_btn /> 
+              {/* “If left side is true → show right side..If left side is false → show nothing” */}
+             </div>
+            )}
+            </ul>
+        </nav>
+      </Container>
+    </header>
   )
 }
 
--- 12Mega_blog/src/components/index.js
+++ 12Mega_blog/src/components/index.js
@@ -1,7 +1,12 @@
 import Header from "./Header/header";
 import Footer from "./Footer/footer";
-
+import containter from "./container/container"
+import Logo from "./Logo";
+import Logout_btn from "./Header/Logout_btn"
 export {
     Header,
-    Footer
+    Footer,
+    containter,
+    Logo,
+    Logout_btn
 }
